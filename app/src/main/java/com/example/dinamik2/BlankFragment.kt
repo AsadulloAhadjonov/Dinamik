e26f2c9b1d8f1b6562391855820aafc507163a46
Asadullo <asadulloahadjonovpubg@gmail.com>
2023-07-17 14:31:30 +0500

    Dinamik

---
+++ app/src/main/java/com/example/dinamik2/BlankFragment.kt
@@ -0,0 +1,33 @@
+package com.example.dinamik2
+
+import android.os.Bundle
+import android.text.Editable
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class BlankFragment : Fragment() {
+
+    private lateinit var root:View
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        root = inflater.inflate(R.layout.fragment_blank, container, false)
+
+        var text = root.findViewById<TextView>(R.id.txt_1)
+        var btn = root.findViewById<Button>(R.id.btn_add)
+        btn.setOnClickListener {
+            text.text = "Hello"
+        }
+
+        return root
+    }
+
+
+}
